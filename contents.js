7066802abbd63c543793a9896aea58bb5344ef67
Francesco Catania <81304650+FrancescoCt@users.noreply.github.com>
2021-04-07 23:54:14 +0200

    Add files via upload

---
+++ contents.js
@@ -0,0 +1,59 @@
+const contenuti = [
+	{
+		codice: '1',
+		immagine: "magliettaRagazzo.jfif",
+		nome: 'Maglietta',
+		genere: ' ragazzo',
+		taglia: ' L ',
+		prezzo: '20€'
+	},
+	
+	{
+		codice: '2',
+		immagine: "magliettaRagazzo1.jfif",
+		nome: 'Maglietta',
+		genere: ' ragazzo',
+		taglia: ' S ',
+		prezzo: '15€'
+	},
+	{
+		codice: '3',
+		immagine: "felpa.jfif",
+		nome: 'Felpa',
+		genere: ' ragazzo',
+		taglia: ' L ',
+		prezzo: '25€'
+	},
+	{
+		codice: '4',
+		immagine: "magliettaRagazza.jfif",
+		nome: 'Maglietta',
+		genere: ' ragazza',
+		taglia: ' M ',
+		prezzo: '18€'
+	},
+	{
+		codice: '5',
+		immagine: "repartoUomo.jpeg",
+		nome: 'Reparto',
+		genere: ' Uomo',
+		indirizzo: ' Via Filomena 48- Milano'
+		
+	},
+	{
+		codice: '6',
+		immagine: "repartoDonna.jpg",
+		nome: 'Reparto',
+		genere: ' Donna',
+		indirizzo: ' Via Filomena 48 - Milano'
+		
+	},
+	{
+		codice: '7',
+		immagine: "repartoRagazzo.jpg",
+		nome: 'Reparto',
+		genere: ' Ragazzo',
+		indirizzo: ' Via Capuana 17 - Milano'
+		
+	}
+]
