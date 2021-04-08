f37b420d8f198cd76170797cd5ac74b5f9d83c9a
Francesco Catania <81304650+FrancescoCt@users.noreply.github.com>
2021-04-08 12:12:44 +0200

    Add files via upload

---
+++ contentsGalleria.js
@@ -0,0 +1,102 @@
+const contenuti = [
+	{
+		codice: '1',
+		immagine: "magliettaRagazzo.jfif",
+		nome: 'Maglietta',
+		genere: ' ragazzo',
+		taglia: ' L ',
+		prezzo: '20€'
+	},
+	
+	{
+		codice: '2',
+		immagine: "magliettaRagazzo1.jfif",
+		nome: 'Maglietta',
+		genere: ' ragazzo',
+		taglia: ' S ',
+		prezzo: '15€'
+	},
+	{
+		codice: '3',
+		immagine: "felpa.jfif",
+		nome: 'Felpa',
+		genere: ' ragazzo',
+		taglia: ' L ',
+		prezzo: '25€'
+	},
+	{
+		codice: '4',
+		immagine: "magliettaRagazza.jfif",
+		nome: 'Maglietta',
+		genere: ' ragazza',
+		taglia: ' M ',
+		prezzo: '18€'
+	},
+	{
+		codice: '5',
+		immagine: "magliettaRagazzo.jfif",
+		nome: 'Maglietta',
+		genere: ' ragazzo',
+		taglia: ' L ',
+		prezzo: '20€'
+	},
+	
+	{
+		codice: '6',
+		immagine: "magliettaRagazzo1.jfif",
+		nome: 'Maglietta',
+		genere: ' ragazzo',
+		taglia: ' S ',
+		prezzo: '15€'
+	},
+	{
+		codice: '7',
+		immagine: "felpa.jfif",
+		nome: 'Felpa',
+		genere: ' ragazzo',
+		taglia: ' L ',
+		prezzo: '25€'
+	},
+	{
+		codice: '8',
+		immagine: "magliettaRagazza.jfif",
+		nome: 'Maglietta',
+		genere: ' ragazza',
+		taglia: ' M ',
+		prezzo: '18€'
+	},
+	{
+		codice: '9',
+		immagine: "magliettaRagazzo.jfif",
+		nome: 'Maglietta',
+		genere: ' ragazzo',
+		taglia: ' L ',
+		prezzo: '20€'
+	},
+	
+	{
+		codice: '10',
+		immagine: "magliettaRagazzo1.jfif",
+		nome: 'Maglietta',
+		genere: ' ragazzo',
+		taglia: ' S ',
+		prezzo: '15€'
+	},
+	{
+		codice: '11',
+		immagine: "felpa.jfif",
+		nome: 'Felpa',
+		genere: ' ragazzo',
+		taglia: ' L ',
+		prezzo: '25€'
+	},
+	{
+		codice: '12',
+		immagine: "magliettaRagazza.jfif",
+		nome: 'Maglietta',
+		genere: ' ragazza',
+		taglia: ' M ',
+		prezzo: '18€'
+	}
+	
+	]
---
+++ contentsReparti.js
@@ -0,0 +1,75 @@
+const contenuti = [
+	{
+		codice: '1',
+		immagine: "repartoUomo.jpeg",
+		nome: 'Reparto',
+		genere: ' Uomo',
+		indirizzo: ' Via Filomena 48- Milano'
+		
+	},
+	{
+		codice: '2',
+		immagine: "repartoDonna.jpg",
+		nome: 'Reparto',
+		genere: ' Donna',
+		indirizzo: ' Via Filomena 48 - Milano'
+		
+	},
+	{
+		codice: '3',
+		immagine: "repartoRagazzo.jpg",
+		nome: 'Reparto',
+		genere: ' Ragazzo',
+		indirizzo: ' Via Capuana 17 - Milano'
+		
+	},
+	{
+		codice: '4',
+		immagine: "repartoUomo.jpeg",
+		nome: 'Reparto',
+		genere: ' Uomo',
+		indirizzo: ' Via Filomena 48- Milano'
+		
+	},
+	{
+		codice: '5',
+		immagine: "repartoDonna.jpg",
+		nome: 'Reparto',
+		genere: ' Donna',
+		indirizzo: ' Via Filomena 48 - Milano'
+		
+	},
+	{
+		codice: '6',
+		immagine: "repartoRagazzo.jpg",
+		nome: 'Reparto',
+		genere: ' Ragazzo',
+		indirizzo: ' Via Capuana 17 - Milano'
+		
+	},
+	{
+		codice: '7',
+		immagine: "repartoUomo.jpeg",
+		nome: 'Reparto',
+		genere: ' Uomo',
+		indirizzo: ' Via Filomena 48- Milano'
+		
+	},
+	{
+		codice: '8',
+		immagine: "repartoDonna.jpg",
+		nome: 'Reparto',
+		genere: ' Donna',
+		indirizzo: ' Via Filomena 48 - Milano'
+		
+	},
+	{
+		codice: '9',
+		immagine: "repartoRagazzo.jpg",
+		nome: 'Reparto',
+		genere: ' Ragazzo',
+		indirizzo: ' Via Capuana 17 - Milano'
+		
+	}
+	
+]
